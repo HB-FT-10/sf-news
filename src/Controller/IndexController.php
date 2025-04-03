137a89eb34cd788e8d50a8fbf8111b8b6695b8dd
ld-web <lucas@ld-web.net>
2025-04-03 15:10:43 +0200

    :sparkles: Homepage, about, contact

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class IndexController extends AbstractController
+{
+    #[Route('/', name: 'homepage')]
+    public function home(): Response
+    {
+        return $this->render('index/home.html.twig');
+    }
+
+    #[Route('/about', name: 'app_about')]
+    public function about(): Response
+    {
+        return $this->render('index/about.html.twig');
+    }
+
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(): Response
+    {
+        return $this->render('index/contact.html.twig', [
+            'controller_name' => 'ContactController',
+        ]);
+    }
+}
