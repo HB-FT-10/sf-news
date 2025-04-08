1cdae28051d0b51ccf12efaa6667f092315df0f1
ld-web <lucas@ld-web.net>
2025-04-08 16:49:52 +0200

    :sparkles: Category fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Article;
+use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
@@ -10,18 +11,30 @@ use Faker\Factory;
 class AppFixtures extends Fixture
 {
     private const NB_ARTICLES = 40;
+    private const CATEGORY_NAMES = ['PHP', 'Symfony', 'Rust', 'Typescript', 'Angular', 'Javascript'];
 
     public function load(ObjectManager $manager): void
     {
+        $categories = [];
+
+        foreach (self::CATEGORY_NAMES as $categoryName) {
+            $category = new Category();
+            $category->setName($categoryName);
+            $manager->persist($category);
+            $categories[] = $category;
+        }
+
         $faker = Factory::create('zh_TW');
 
         for ($i = 0; $i < self::NB_ARTICLES; $i++) {
             $article = new Article();
-            $article->setTitle($faker->realText(15));
-            $article->setDescription($faker->text(150));
-            $article->setContent($faker->realTextBetween(400, 750));
-            $article->setCreatedAt($faker->dateTimeBetween('-2 years'));
-            $article->setVisible($faker->boolean(80));
+            $article
+                ->setTitle($faker->realText(15))
+                ->setDescription($faker->text(150))
+                ->setContent($faker->realTextBetween(400, 750))
+                ->setCreatedAt($faker->dateTimeBetween('-2 years'))
+                ->setVisible($faker->boolean(80))
+                ->setCategory($faker->randomElement($categories));
 
             $manager->persist($article);
         }
