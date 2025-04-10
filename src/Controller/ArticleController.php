77a6a5ba6f2b4501d6a92e072f68903556079841
ld-web <lucas@ld-web.net>
2025-04-10 10:13:19 +0200

    :sparkles: add Article item page

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ArticleController extends AbstractController
@@ -17,5 +20,11 @@ final class ArticleController extends AbstractController
         return $this->render('articles/list.html.twig', [
             'articles' => $articles
         ]);
+    }
+
+    #[Route('/articles/{id}', name: 'articles_item')]
+    public function item(Article $article): Response
+    {
+        return $this->render('articles/item.html.twig', ['article' => $article]);
     }
 }
