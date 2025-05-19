255742960e6d6e4bd5970f49722cb621e9130e74
ld-web <lucas@ld-web.net>
2025-05-19 10:50:26 +0200

    :sparkles: add Spam Check on Newsletter email

--- src/Controller/NewsletterController.php
+++ src/Controller/NewsletterController.php
@@ -7,9 +7,11 @@ use App\Form\NewsletterType;
 use App\Mail\NewsletterSubscribedConfirmation;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 final class NewsletterController extends AbstractController
 {
@@ -17,7 +19,8 @@ final class NewsletterController extends AbstractController
     public function subscribe(
         Request $request,
         EntityManagerInterface $em,
-        NewsletterSubscribedConfirmation $confirmationService
+        NewsletterSubscribedConfirmation $confirmationService,
+        HttpClientInterface $spamChecker
     ): Response {
         $newsletter = new Newsletter();
         $form = $this->createForm(NewsletterType::class, $newsletter);
@@ -25,15 +28,28 @@ final class NewsletterController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // enregistrer mon email
-            // persist + flush
-            $em->persist($newsletter);
-            $em->flush();
+            // Spam ?
+            $response = $spamChecker->request(Request::METHOD_POST, "/api/check", [
+                'json' => ['email' => $newsletter->getEmail()]
+            ]);
 
-            $confirmationService->sendEmail($newsletter);
+            $data = $response->toArray();
 
-            // Ajout d'un message de succès (notification)
-            $this->addFlash('success', 'Votre inscription a bien été prise en compte, un email de confirmation vous a été envoyé');
+            if ($data['result'] === 'spam') {
+                $form->addError(new FormError("L'email renseigné est un spam"));
+            }
+
+            if ($data['result'] === 'ok') {
+                // enregistrer mon email
+                // persist + flush
+                $em->persist($newsletter);
+                $em->flush();
+
+                $confirmationService->sendEmail($newsletter);
+
+                // Ajout d'un message de succès (notification)
+                $this->addFlash('success', 'Votre inscription a bien été prise en compte, un email de confirmation vous a été envoyé');
+            }
         }
 
         return $this->render('newsletter/subscribe.html.twig', [
